0e028393cfebdb058dd6655458f8751f553725e5
Dorothy John <dorothyy.john@gmail.com>
2020-04-10 21:17:43 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,8 @@
 
 #Define a method with 2 required parameters
 #& 1 optional parameter
+#print 1 string  #print & return 1 string
 def meal_choice(veg1, veg2, protein = "tofu" )
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  
+  p "A plate of #{protein} with #{veg1} and #{veg2}."
+end
